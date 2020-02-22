6688efe6d68108aac02d3f866ce421404160fcad
ArcDrive <alricost@gmail.com>
2020-02-22 14:01:47 -0800

    meh

---
+++ commands/fun/bounty.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "bounty",
+    category: "fun",
+    description: "Shoots some fools",
+    run: async (client, message, args) => {
+        const msg = await message.channel.send(`https://www.youtube.com/watch?v=bs8CsZe4dmA`);
+    }
+}
